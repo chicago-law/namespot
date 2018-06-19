a831fe190822a1a9d015afac04b4d6a0ee7f5d21
elramus <luke.ramus@gmail.com>
2018-06-19 13:29:17 -0500

    edit rooms for single offerings, give chart card style

--- app/Offering.php
+++ app/Offering.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Offering extends Model
 {
+    protected $fillable = ['room_id','course_num'];
+
     public function instructors()
     {
         return $this->belongsToMany('App\Instructor');
@@ -18,6 +20,6 @@ class Offering extends Model
 
     public function students()
     {
-        return $this->belongsToMany('App\Student');
+        return $this->belongsToMany('App\Student')->withPivot('assigned_seat');
     }
 }
