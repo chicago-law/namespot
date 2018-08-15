d2be9fda22209bcafa82d70934e7d693ea114a4f
elramus <luke.ramus@gmail.com>
2018-08-15 11:30:15 -0500

    composer req changes

--- app/Student.php
+++ app/Student.php
@@ -3,7 +3,6 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use Laravel\Scout\Searchable;
 
 class Student extends Model
 {
--- config/app.php
+++ config/app.php
@@ -158,8 +158,6 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Laravel\Scout\ScoutServiceProvider::class,
-        TeamTNT\Scout\TNTSearchScoutServiceProvider::class,
 
         /*
          * Package Service Providers...
