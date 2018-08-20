2bbc3af247ccea48bf1ed928f28177db92c2d6ee
elramus <luke.ramus@gmail.com>
2018-08-20 15:19:02 -0500

    remove label position whitespace from DB

--- app/Http/Resources/Table.php
+++ app/Http/Resources/Table.php
@@ -18,7 +18,7 @@ class Table extends JsonResource
             'id' => $this->id,
             'room_id' => $this->room_id,
             'seat_count' => $this->seat_count,
-            'label_position' => $this->label_position,
+            'label_position' => is_null($this->label_position) ? null : trim($this->label_position),
             'sX' => $this->sX,
             'sY' => $this->sY,
             'eX' => $this->eX,
