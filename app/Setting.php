b403be4d1b8228d005eee8a4ef6ccb290d32438d
elramus <luke.ramus@gmail.com>
2018-08-22 14:28:47 -0500

    Remove primary key requirement for Setting
    The Setting model for the settings table doesn't need an ID / primary key.
    Changed its model class to reflect that.

--- app/Setting.php
+++ app/Setting.php
@@ -8,4 +8,8 @@ class Setting extends Model
 {
     protected $fillable = ['setting_name','setting_value'];
     public $timestamps = false;
+
+    // No primary key on the setting's table
+    protected $primaryKey = null;
+    public $incrementing = false;
 }
