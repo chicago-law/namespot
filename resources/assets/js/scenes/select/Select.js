13bafa1259bcfe204bbd5b940d87930ef0e64fd7
elramus <luke.ramus@gmail.com>
2018-08-28 10:55:54 -0500

    Change Select to stateless

--- resources/assets/js/scenes/select/Select.js
+++ resources/assets/js/scenes/select/Select.js
@@ -1,26 +1,20 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import RoomList from './containers/RoomList'
 import OfferingList from './containers/OfferingList'
 import StudentList from './containers/StudentList'
 import Settings from './containers/Settings'
 
-export default class Select extends Component {
-  constructor(props) {
-    super(props)
-  }
+const Select = () => (
+  <div className='select card narrow-wrap'>
+    <Switch>
+      <Route exact path="/" component={OfferingList} />
+      <Route path="/select/offerings" component={OfferingList} />
+      <Route path="/select/rooms" component={RoomList} />
+      <Route path="/select/students" component={StudentList} />
+      <Route path="/select/settings" component={Settings} />
+    </Switch>
+  </div>
+)
 
-  render() {
-    return (
-      <div className='select card narrow-wrap'>
-        <Switch>
-          <Route exact path="/" component={OfferingList} />
-          <Route path="/select/offerings" component={OfferingList} />
-          <Route path="/select/rooms" component={RoomList} />
-          <Route path="/select/students" component={StudentList} />
-          <Route path="/select/settings" component={Settings} />
-        </Switch>
-      </div>
-    )
-  }
-}
+export default Select
