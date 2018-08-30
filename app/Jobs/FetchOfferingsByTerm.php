825a47dc07c70001314f331da73495922ee3e33a
elramus <luke.ramus@gmail.com>
2018-08-30 16:06:47 -0500

    Switch FetchOfferingsByTerm to staging url

--- app/Jobs/FetchOfferingsByTerm.php
+++ app/Jobs/FetchOfferingsByTerm.php
@@ -178,6 +178,7 @@ class FetchOfferingsByTerm implements ShouldQueue
       if ($e->hasResponse()) {
         $api_response = Psr7\str($e->getResponse());
       } else {
+        dd($e);
         $api_response = 'no response';
       }
 
