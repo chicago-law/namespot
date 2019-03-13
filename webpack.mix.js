3b07fe7810bda0fcc8be50475a2c2ca42fd15659
elramus <luke.ramus@gmail.com>
2019-03-13 10:19:30 -0500

    Change back to "resources"

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,8 +11,8 @@ const mix = require('laravel-mix') //eslint-disable-line
  |
  */
 
-mix.ts('frontend/js/index.js', 'public/js')
-  .sass('frontend/sass/main.scss', 'public/css')
-  .copyDirectory('frontend/images', 'public/images')
+mix.ts('resources/js/index.js', 'public/js')
+  .sass('resources/sass/main.scss', 'public/css')
+  .copyDirectory('resources/images', 'public/images')
   .sourceMaps()
   .browserSync()
