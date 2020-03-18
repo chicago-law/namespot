2dac863dd71d7aec3dc55ba2b669a3ff671df3cf
elramus <luke.ramus@gmail.com>
2020-03-18 08:33:46 -0500

    Move FA to its own file

--- resources/js/root.tsx
+++ resources/js/root.tsx
@@ -3,94 +3,14 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import {
-  faArrowLeft,
-  faArrowToBottom,
-  faAsterisk,
-  faBars,
-  faBookOpen,
-  faCalendarStar,
-  faCheckCircle,
-  faChevronRight,
-  faCog,
-  faFile,
-  faFolderOpen,
-  faIdCard,
-  faLongArrowUp,
-  faLongArrowDown,
-  faLongArrowLeft,
-  faLongArrowRight,
-  faMap,
-  faMapMarkerAlt,
-  faPencil,
-  faPlusCircle,
-  faPrint,
-  faRobot,
-  faSearch,
-  faSync,
-  faTable,
-  faTextSize,
-  faTimes,
-  faTrashAlt,
-  faUniversity,
-  faUnlink,
-  faUpload,
-  faUserEdit,
-  faUserPlus,
-  faUserTag,
-  faUserTimes,
-  faUsers,
-  faWrench,
-} from '@fortawesome/pro-regular-svg-icons'
-import {
-  faCamera,
-} from '@fortawesome/pro-solid-svg-icons'
 import { ThemeProvider } from './utils/styledComponents'
 import { theme } from './utils/theme'
 import App from './components/App'
 import store from './store'
 import GlobalStyles from './utils/globalStyles'
+import { faIcons } from './utils/fa'
 
-library.add(
-  faArrowLeft,
-  faArrowToBottom,
-  faAsterisk,
-  faBars,
-  faBookOpen,
-  faCalendarStar,
-  faCamera,
-  faCheckCircle,
-  faChevronRight,
-  faCog,
-  faFile,
-  faFolderOpen,
-  faIdCard,
-  faLongArrowUp,
-  faLongArrowDown,
-  faLongArrowLeft,
-  faLongArrowRight,
-  faMap,
-  faMapMarkerAlt,
-  faPencil,
-  faPlusCircle,
-  faPrint,
-  faRobot,
-  faSearch,
-  faSync,
-  faTable,
-  faTextSize,
-  faTimes,
-  faTrashAlt,
-  faUniversity,
-  faUnlink,
-  faUpload,
-  faUserEdit,
-  faUserPlus,
-  faUserTag,
-  faUserTimes,
-  faUsers,
-  faWrench,
-)
+library.add(...faIcons)
 
 ReactDOM.render(
   <BrowserRouter>
